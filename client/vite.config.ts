475affcebe3ce9ee9e078b4b77d4e52344d13aad
seank75 <poly7578@gmail.com>
2026-03-04 12:52:45 +0900

    fix: split phaser chunk to resolve netlify build size limits

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,5 +3,15 @@ import { defineConfig } from "vite";
 export default defineConfig({
     server: {
         host: true
+    },
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    phaser: ['phaser']
+                }
+            }
+        },
+        chunkSizeWarningLimit: 1500
     }
 });
